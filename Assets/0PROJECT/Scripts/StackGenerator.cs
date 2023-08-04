281321cf4e041499492ddb6b9c342a17a864336b
Avenuxx <cavdarsinan50@gmail.com>
2023-08-04 18:25:54 +0300

    split stacks

--- Assets/0PROJECT/Scripts/StackGenerator.cs
+++ Assets/0PROJECT/Scripts/StackGenerator.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class StackGenerator : MonoBehaviour
 {
+    public Material[] stackMaterials;
+    public int score;
+
+    [SerializeField]
+    private StackMovement stackPrefab;
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)||Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
-            StackMovement.CurrentStack.Stop();
+            if (StackMovement.CurrentStack != null)
+                StackMovement.CurrentStack.Stop();
+
+            SpawnStack();
         }
+    }
+
+    private void SpawnStack()
+    {
+        var stack = Instantiate(stackPrefab);
+
+        if (StackMovement.LastStack != null && StackMovement.LastStack.gameObject != GameObject.Find("StartStack"))
+        {
+            int xPos = score % 2==0 ? -4 : 4;
+            stack.transform.position = new Vector3(xPos, transform.position.y, StackMovement.LastStack.transform.position.z + stackPrefab.transform.localScale.z);
+        }
+        else
+        {
+            stack.transform.position = transform.position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, stackPrefab.transform.localScale);
     }
 }
--- Assets/0PROJECT/Scripts/StackMovement.cs
+++ Assets/0PROJECT/Scripts/StackMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class StackMovement : MonoBehaviour
 {
     public float moveSpeed = 1f;
+    StackGenerator stackGenerator;
+    private bool _isForward = true;
 
     public static StackMovement CurrentStack { get; private set; }
     public static StackMovement LastStack { get; private set; }
@@ -16,26 +18,42 @@ public class StackMovement : MonoBehaviour
             LastStack = GameObject.Find("StartStack").GetComponent<StackMovement>();
 
         CurrentStack = this;
+        stackGenerator = FindObjectOfType<StackGenerator>();
+        GetComponent<Renderer>().material = stackGenerator.stackMaterials[stackGenerator.score];
+
+        transform.localScale = new Vector3(LastStack.transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
     public void Stop()
     {
         moveSpeed = 0;
         float diff = transform.position.x - LastStack.gameObject.transform.position.x;
-        SplitCube(diff);
+
+
+        if (Mathf.Abs(diff) >= LastStack.transform.localScale.x)
+        {
+            LastStack = null;
+            CurrentStack = null;
+            Debug.Log("Game over");
+            return;
+        }
+
+        float dir = diff > 0 ? 1 : -1;
+        SplitCube(diff, dir);
+        LastStack = GetComponent<StackMovement>();
     }
 
-    private void SplitCube(float diff)
+    private void SplitCube(float diff, float dir)
     {
-        float newBoundsSize = LastStack.transform.position.x - Mathf.Abs(diff);
+        float newBoundsSize = LastStack.transform.localScale.x - Mathf.Abs(diff);
         float fallingStackSize = transform.localScale.x - newBoundsSize;
 
         float newBlockPosX = LastStack.transform.position.x + (diff / 2);
         transform.localScale = new Vector3(newBoundsSize, transform.localScale.y, transform.localScale.z);
         transform.position = new Vector3(newBlockPosX, transform.position.y, transform.position.z);
 
-        float stackEdge = transform.position.z + newBoundsSize / 2f;
-        float fallingStackXPos = stackEdge + fallingStackSize / 2f;
+        float stackEdge = transform.position.x + (newBoundsSize / 2f * dir);
+        float fallingStackXPos = stackEdge + fallingStackSize / 2f * dir;
 
         SpawnFallingStack(fallingStackXPos, fallingStackSize);
     }
@@ -45,10 +63,28 @@ public class StackMovement : MonoBehaviour
         var newStack = GameObject.CreatePrimitive(PrimitiveType.Cube);
         newStack.transform.localScale = new Vector3(fallingStackSize, transform.localScale.y, transform.localScale.z);
         newStack.transform.position = new Vector3(fallingStackXPos, transform.position.y, transform.position.z);
+
+        newStack.AddComponent<Rigidbody>();
+        newStack.GetComponent<Renderer>().material = GetComponent<Renderer>().material;
+        stackGenerator.score++;
+        Destroy(newStack, 5f);
     }
 
     void Update()
     {
-        transform.position += transform.right * Time.deltaTime * moveSpeed;
+        var position = transform.position;
+        var direction = _isForward ? 1 : -1;
+        var move = moveSpeed * Time.deltaTime * direction;
+        var limit = LastStack.transform.localScale.x + 1;
+
+        position.x += move;
+
+        if (position.x < -limit || position.x > limit)
+        {
+            position.x = Mathf.Clamp(position.x, -limit, limit);
+            _isForward = !_isForward;
+        }
+
+        transform.position = position;
     }
 }
